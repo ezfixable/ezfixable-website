b787057e2fad28f87a22c77de07c3f0befa9d2b7
ezfixable <ezfixable@gmail.com>
2025-09-30 23:03:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React from "react";
 
+function App() {
+  return (
+    <div>
+      <h1>Appliance & Commercial Oven Repair</h1>
+      <p>Same-day when possible. Transparent pricing.</p>
+    </div>
+  );
+}
+
+export default App;
